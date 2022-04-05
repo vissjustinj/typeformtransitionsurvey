3ad822477a05b25cd8ea8ef4a08b4c9ac96808a3
Justin Viss <vissjustinj@gmail.com>
2022-04-05 10:53:27 -0500

    change front page

--- src/GetStarted.js
+++ src/GetStarted.js
@@ -10,10 +10,10 @@ const GetStarted = (props) => {
         <div>
             <div className="ui raised segment" id="getstarted" title="Automatically Write HS Transition Survey">
                 <h1>Automatically Write HS Transition Survey</h1>
-                <h3>Write your report while you're interviewing your students about their post-high school plans</h3>
-                <h3>Here's how it works:</h3>
-                <iframe width="500" height="400" id="getstartedvid"src="https://www.youtube.com/embed/6JL2htnXq2g" title="Automatically Write HS Transition Survey" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <h3>Write your report while you're interviewing students about their post-high school plans</h3>
                 <button className="ui button" onClick={handleStart} style={{maxWidth:"150px",alignSelf:"center"}}>Get Started</button>
+                <iframe width="60%" height="400" id="getstartedvid"src="https://www.youtube.com/embed/6JL2htnXq2g" title="Automatically Write HS Transition Survey" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                
             </div>
             
         </div>

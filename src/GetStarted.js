d6f936334a4be43b94ff816d3b6a4b51d4a5ce9e
Justin Viss <vissjustinj@gmail.com>
2022-04-05 08:16:29 -0500

    added getstarted video

--- src/GetStarted.js
+++ src/GetStarted.js
@@ -10,7 +10,9 @@ const GetStarted = (props) => {
         <div>
             <div className="ui raised segment" id="getstarted">
                 <h1>Automatically Write HS Transition Survey</h1>
-                <h3></h3>
+                <h3>Write your report while you're interviewing your students about their post-high school plans</h3>
+                <h3>Here's how it works:</h3>
+                <iframe width="500" height="400" id="getstartedvid"src="https://www.youtube.com/embed/6JL2htnXq2g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 <button className="ui button" onClick={handleStart} style={{maxWidth:"150px",alignSelf:"center"}}>Get Started</button>
             </div>
             
@@ -20,4 +22,3 @@ const GetStarted = (props) => {
 
 export default GetStarted
 
-//<iframe width="500" height="400" id="getstartedvid"src="https://www.youtube.com/embed/fRHKOof9MwY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>

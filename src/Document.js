61b609f35ecbf33798a872078b4277001c412bea
Justin Viss <vissjustinj@gmail.com>
2022-04-05 08:36:23 -0500

    changed iframe title for document

--- src/Document.js
+++ src/Document.js
@@ -9,6 +9,7 @@ const Document = () => {
             <iframe src="https://docs.google.com/document/d/e/2PACX-1vSya7ga0pf52F3SN9My86VKph0CtJ5OX2tc8dtGIqEacV4zckk1LmkYIsNuKQOcSitwpkEc5ouF77y1/pub?embedded=true"
             style={{width:"100%"}}
             id="doc"
+            title="The Template"
             >
             </iframe>
         </div>

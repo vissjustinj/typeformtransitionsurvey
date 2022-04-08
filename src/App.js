66e6da6df04bccd12b1c64ab810adc4b321ba9d5
Justin Viss <vissjustinj@gmail.com>
2022-04-08 09:48:36 -0500

    add GA tag, add reload button, alter dimensions on getstarted video

--- src/App.js
+++ src/App.js
@@ -1,42 +1,50 @@
 import './App.css';
-import React,{useState} from 'react'
+import React, { useState } from 'react';
 import Document from './Document';
 import Typeform from './Typeform';
-import GetStarted from './GetStarted'
+import GetStarted from './GetStarted';
 function App() {
-  
-  const [start, setStart] = useState(false)
-  
-  const getStarted = () => {
-    setStart(true)
-  }
-  
-  if (start ===false) {
-    return(
-      <GetStarted onStart={getStarted}/>
-    )
-  }
-  
-  return (
-    <div >
-         <h1 style={{textAlign:"center", padding:"10px"}}>Transitions Survey</h1>
-        <div className="App" >
-          <div className="ui grid" style={{height:"100%"}}>
-            <div className="eight wide column" >
-                <div className="ui raised segment" style={{height:"100%"}}>
-                  <Typeform style={{height:"100%"}}/>
-                </div>
-            </div>
-            <div className="eight wide column">
-              <div className="ui raised segment" style={{height:"100%"}}>
-                <Document />
-              </div>
-            </div>
-          </div>
-        </div>
-    </div>
-   
-  );
+	const [ start, setStart ] = useState(false);
+
+	const getStarted = () => {
+		setStart(true);
+	};
+
+	if (start === false) {
+		return <GetStarted onStart={getStarted} />;
+	}
+
+	return (
+		<div>
+			<div>
+				<h1 style={{ textAlign: 'center', padding: '10px' }}>
+					Transitions Survey{' '}
+					<button
+						style={{ fontSize: 'medium' }}
+						onClick={() => {
+							window.location.reload(false);
+						}}
+					>
+						Reload Page
+					</button>
+				</h1>
+			</div>
+			<div className="App">
+				<div className="ui grid" style={{ height: '100%' }}>
+					<div className="eight wide column">
+						<div className="ui raised segment" style={{ height: '100%' }}>
+							<Typeform style={{ height: '100%' }} />
+						</div>
+					</div>
+					<div className="eight wide column">
+						<div className="ui raised segment" style={{ height: '100%' }}>
+							<Document />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/Typeform.js
+++ src/Typeform.js
@@ -1,14 +1,13 @@
-import React from 'react'
-import {Widget} from '@typeform/embed-react'
+import React from 'react';
+import { Widget } from '@typeform/embed-react';
 
 const Typeform = () => {
+	return (
+		<div style={{ height: '100%' }}>
+			<h2>The Survey</h2>
+			<Widget id="AzkmxzSB" style={{ width: '100%', height: '90%' }} className="my-form" />
+		</div>
+	);
+};
 
-    return(
-        <div style={{height:"100%"}}>
-            <h2>The Survey</h2>
-            <Widget id="AzkmxzSB" style={{width:"100%",height:"100%"}} className="my-form"/>
-        </div>
-    )
-}
-
-export default Typeform
+export default Typeform;
